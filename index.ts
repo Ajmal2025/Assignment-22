ef00092fa89ea8912ddbe6898998c2fb057211e6
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 22:08:44 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+// 22.	Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+//  Error in code due to index
+const alphabet1:string[]=["A","L","C","M"]
+
+
+console.log(alphabet1.indexOf('1))
+
+// so last index 2 
+console.log(`${alphabet1[5]}`)
